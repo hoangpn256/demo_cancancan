f85b57b3d44d2be781e2c39de7ca0faa438885e0
Pham Ngoc Hoang <pnhoang.bk@gmail.com>
2020-01-30 17:32:04 +0700

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to root_path, notice: exception.message }
+      format.js { head :forbidden, content_type: 'text/html' }
+    end
+  end
+end
---
+++ app/views/posts/_post.json.jbuilder
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+json.extract! post, :id, :title, :content, :created_at, :updated_at, :user_id
+json.url post_url(post, format: :json)
